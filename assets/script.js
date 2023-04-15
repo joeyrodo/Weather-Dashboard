41291d9cdf719a318fd17ccff9b3fb3342a29b02
joeyrodo <joeyrodo@gmail.com>
2023-04-15 09:46:55 -0400

    Added 5DayWeather API

--- assets/script.js
+++ assets/script.js
@@ -11,30 +11,33 @@ function submitted() {
     userInput = document.getElementById("text-box").value;
     city = userInput;
     if (userInput != "") {
-    console.log("input detected!");
-    console.log(city);
-    var geocodeURL = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=" + 1 + "&appid=" + APIKey;
-    fetch(geocodeURL).then((response) => response.json()).then((geocode) => {
-        geocodeName = geocode[0].name;
-        lat = geocode[0].lat;
-        lon = geocode[0].lon;
-        geocodeTest();
-    });
-    var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
-    fetch(queryURL).then((response) => response.json()).then((forecast) => {
-        temp = Math.round((forecast.main.temp - 273.15) * 1.8 + 32);
-        testLog();
-    });
+        console.log("input detected!");
+        console.log(city);
+        var geocodeURL = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=" + 1 + "&appid=" + APIKey;
+        fetch(geocodeURL).then((response) => response.json()).then((geocode) => {
+            geocodeName = geocode[0].name;
+            lat = geocode[0].lat;
+            lon = geocode[0].lon;
+            geocodeTest();
+            latLonSubmit();
+        });
 
-    function testLog() {
-        console.log(temp);
-    
-    }
+        function latLonSubmit() {
+            var forecastURL = "http://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + APIKey;
+            fetch(forecastURL).then((response) => response.json()).then((forecast) => {
+                temp = forecast;
+                testLog();
+            });
+        }
+        function testLog() {
+            console.log(temp);
 
-    function geocodeTest() {
-        console.log("geocode test successful!");
-        console.log(geocodeName, lat, lon);
-    }
-    console.log("hi");
+        }
+
+        function geocodeTest() {
+            console.log("geocode test successful!");
+            console.log(geocodeName, lat, lon);
+        }
+        console.log("hi");
     }
 }
